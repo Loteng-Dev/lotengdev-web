6f4e2446029cba4d33d5626f32ff695653d29556
Vanessa Cassandra <vanessa.cassandra@juliusbaer.com>
2022-10-26 18:22:54 +0700

    Add Navbar to Home screen (#3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      blue: "#007BFF",
+      grey: "#C0C0C0",
+      white: "#FFF",
+    },
   },
   plugins: [],
-}
+};
